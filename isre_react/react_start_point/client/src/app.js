c2ffd659c2aee4ba8c906ee4992c6d87a5f8b579
Chris Woodall <christopherhwoodall@gmail.com>
2017-07-24 11:36:37 +0100

    setup react

---
+++ isre_react/react_start_point/client/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.addEventListener('load', function () {
+  ReactDOM.render(
+    <h1>International Repository for Scientific Experimentation </h1>,
+    document.getElementById('app')
+  );
+});
